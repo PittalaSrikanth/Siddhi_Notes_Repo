49a848e582cbdafbbb925ccc2c5c8bf06c00b895
spittala <spittala@capspayroll.com>
2022-04-01 12:55:16 +0530

    01012022

---
+++ Selenium_Practices/javapractice/src/test/practice/Sample.java
@@ -0,0 +1,18 @@
+package test.practice;
+
+public class Sample {
+
+	public static void main(String[] args) 
+	{
+		int a = 10,b= 5;
+		System.out.println(a++);
+		System.out.println(a);
+		
+		boolean c = (a++>10);
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+
+	}
+
+}
