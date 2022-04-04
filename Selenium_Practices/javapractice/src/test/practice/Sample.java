36b755765b1ac79a2efa0e3b88a57fe3e8891d6d
spittala <spittala@capspayroll.com>
2022-04-04 13:20:01 +0530

    04042022

--- Selenium_Practices/javapractice/src/test/practice/Sample.java
+++ Selenium_Practices/javapractice/src/test/practice/Sample.java
@@ -1,18 +1,12 @@
 package test.practice;
 
 public class Sample {
-
 	public static void main(String[] args) 
 	{
-		int a = 10,b= 5;
-		System.out.println(a++);
-		System.out.println(a);
 		
-		boolean c = (a++>10);
-		System.out.println(a);
-		System.out.println(b);
-		System.out.println(c);
-
+		System.out.println(5%3*4/2+5%2+5-2%6);
 	}
+	
+	
 
 }
